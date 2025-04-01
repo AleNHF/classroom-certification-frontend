160763cbce391bf7e87bed5436beb9479eb7f42e
Herbas Fernandez Alejandra Nicole <herbasfernandez.alejandra@gmail.com>
2025-04-01 14:52:03 -0400

    refactor: Add enviroment variables

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,8 +2,8 @@ class AuthService {
     private baseUrl: string;
 
     constructor() {
-        this.baseUrl = 'http://localhost:3000/api';
-        //this.baseUrl = 'https://classroom-certification-api-production.up.railway.app/api';
+        //this.baseUrl = 'http://localhost:3000/api';
+        this.baseUrl = import.meta.env.VITE_BACKEND_URL;
     }
 
     private async request(endpoint: string, options: RequestInit) {
