dc11d31727e9e572f6c01861db883c641a4d0955
Herbas Fernandez Alejandra Nicole <herbasfernandez.alejandra@gmail.com>
2025-04-27 18:11:56 -0400

    refactor: Cambiar a combobox el input de Facultad en Teams

--- src/pages/workTeams/TeamPage.tsx
+++ src/pages/workTeams/TeamPage.tsx
@@ -269,7 +269,7 @@ const TeamPage: React.FC = () => {
                         </div>
                     </div>
 
-                    <div className="mt-4">
+                    {/* <div className="mt-4">
                         <label className="block text-sm font-medium text-gray-700">Facultad</label>
                         <input
                             type="text"
@@ -278,6 +278,25 @@ const TeamPage: React.FC = () => {
                             className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:ring focus:ring-blue-200 focus:border-blue-500"
                         />
                         {formErrors.faculty && <p className="text-red-600 text-sm">{formErrors.faculty}</p>}
+                    </div> */}
+
+                    <div className="mt-3">
+                        <label
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Facultad:
+                        </label>
+                        <select
+                            value={newTeam.faculty}
+                            onChange={(e) => setNewTeam((prev: any) => ({ ...prev, faculty: e.target.value }))}
+                            className="border border-gray-300 rounded-md p-2 w-full mt-2 focus:ring focus:ring-blue-200 focus:border-blue-500"
+                        >
+                            <option value="">Selecciona una facultad</option>
+                            <option value="Facultad de Ingeniería en Ciencias de la Computación y Telecomunicaciones">Facultad de Ingeniería en Ciencias de la Computación y Telecomunicaciones</option>
+                            <option value="Facultad de Ciencias Jurídicas, Políticas, Sociales y Relaciones Internacionales">Facultad de Ciencias Jurídicas, Políticas, Sociales y Relaciones Internacionales</option>
+                            <option value="Facultad de Ciencias Contables, Auditoría, Sistemas de Control de Gestión y Finanzas">Facultad de Ciencias Contables, Auditoría, Sistemas de Control de Gestión y Finanzas</option>
+                        </select>
+                        {formErrors.faculty && <p className="text-red-600 text-sm mt-1">{formErrors.faculty}</p>}
                     </div>
 
                     {/* Miembros del equipo */}
